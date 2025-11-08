11f09ed90cb807d48b682fb9bc4604e9dad5dc16
mmarufqk <mmarufqomaruddinkafi@gmail.com>
2025-11-08 21:50:53 +0700

    lupa blm ke push

--- src/app/soal-3/page.tsx
+++ src/app/soal-3/page.tsx
@@ -3,22 +3,39 @@
 export default function Soal3() {
     return (
         <div className="min-h-screen bg-white flex flex-col">
-            {/* Sun - Top Right */}
-            <div className="absolute top-4 right-4 w-24 h-24 bg-yellow-400 rounded-full shadow-lg"></div>
+            <div className="absolute top-8 right-8 w-32 h-32 bg-yellow-400 rounded-full shadow-lg"></div>
 
-            {/* Main Content */}
             <div className="flex-1 flex flex-col justify-end">
-                <div className="relative h-20">
-                    <div className="flex justify-center space-x-8 absolute bottom-0 w-full">
-                        <div className="w-16 bg-blue-500 rounded-t-full h-12"></div>
-                        <div className="w-16 bg-blue-500 rounded-t-full h-16"></div>
-                        <div className="w-16 bg-blue-500 rounded-t-full h-10"></div>
-                        <div className="w-16 bg-blue-500 rounded-t-full h-14"></div>
-                        <div className="w-16 bg-blue-500 rounded-t-full h-11"></div>
+                <div className="relative h-screen">
+                    <div className="flex justify-between absolute bottom-0 w-full z-0 px-4">
+                        <div className="relative">
+                            <div className="w-56 h-56 bg-blue-500"></div>
+                            <div className="absolute -top-40 left-0 w-56 h-56 bg-white rounded-b-full"></div>
+                        </div>
+                        <div className="relative">
+                            <div className="w-56 h-64 bg-blue-500"></div>
+                            <div className="absolute -top-48 left-0 w-56 h-64 bg-white rounded-b-full"></div>
+                        </div>
+                        <div className="relative">
+                            <div className="w-56 h-48 bg-blue-500"></div>
+                            <div className="absolute -top-32 left-0 w-56 h-48 bg-white rounded-b-full"></div>
+                        </div>
+                        <div className="relative">
+                            <div className="w-56 h-60 bg-blue-500"></div>
+                            <div className="absolute -top-44 left-0 w-56 h-60 bg-white rounded-b-full"></div>
+                        </div>
+                        <div className="relative">
+                            <div className="w-56 h-52 bg-blue-500"></div>
+                            <div className="absolute -top-36 left-0 w-56 h-52 bg-white rounded-b-full"></div>
+                        </div>
                     </div>
                 </div>
 
-                <div className="w-[60%] h-8 bg-amber-900 self-center mb-6 rounded-t-lg relative z-10"></div>
+                {/* Brown Ground Line - paling depan, 20% dari bawah */}
+                <div
+                    className="w-[70%] h-20 bg-amber-900 self-center mb-20 rounded-t-xl fixed bottom-0 z-50"
+                    style={{ bottom: "20%" }}
+                ></div>
             </div>
         </div>
     );
